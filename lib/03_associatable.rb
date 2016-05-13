60948d21214f7485d51ab9c4e3f73b808758917e
makenneth <kenneth.km.ma@gmail.com>
2016-05-13 11:23:14 -0700

    finished associatable

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -20,16 +20,16 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    @foreign_key = options[:foreign_key] || "#{name}_id".underscore.to_sym 
-    @class_name = options[:class_name] || name.camelcase
+    @foreign_key = options[:foreign_key] || "#{name}Id".underscore.to_sym 
+    @class_name = options[:class_name] || name.to_s.camelcase
     @primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    @foreign_key = options[:foreign_key] || "#{self_class_name}_id".underscore.to_sym
-    @class_name = options[:class_name] || name.singularize.camelcase
+    @foreign_key = options[:foreign_key] || "#{self_class_name}Id".underscore.to_sym
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
     @primary_key = options[:primary_key] || :id
   end
 end
@@ -37,15 +37,33 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    self.assoc_options[name] = 
+        BelongsToOptions.new(name, options)
+
+    define_method(name) do 
+      options = self.class.assoc_options[name]
+      foreign_id = self.send(options.foreign_key)
+
+      options.model_class.where(options.primary_key => foreign_id).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] =
+          HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      primary_id = self.send(options.primary_key)
+
+      options.model_class.where(options.foreign_key => primary_id)
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,15 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do 
+    	through_class = self.send(through_name)
+    	through_class.send(source_name)
+    end
+  end
+
+
+  def has_many_through(name, through_name, source_name)
+  	define_method(name) do
+  		through_class = self.send(through_name)
   end
 end
